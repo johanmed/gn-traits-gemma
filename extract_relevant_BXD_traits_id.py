14550903236ee74359f3154be3fd34268f9b49fa
Johannes Medagbe <johanmedagbe@gmail.com>
2024-09-28 15:43:18 +0300

    BXD Phenotype file modified and only relevant traits retrieved

--- extract_relevant_BXD_traits_id.py
+++ extract_relevant_BXD_traits_id.py
@@ -2,11 +2,14 @@
 
 import re
 
-list_keywords=['immune', 'Immune', 'metabolism', 'Metabolism', 'obesity', 'Obesity', 'Metagenome, microbiota, gastrointestinal system']
+list_keywords=['immune', 'Immune', 'metabolism', 'Metabolism', 'obesity', 'Obesity', 'Metagenome, microbiota, gastrointestinal system'] # list of keywords related to project
 
 def extract_traits(phenotype_filename, list_keywords):
     """
-    
+    Takes phenotype metadata file and list of keywords related to project
+    Retrieves trait description and id
+    Saves in a dictionary container with an unique key corresponding to full description of trait
+    Returns dictionary container
     """
     container={}
     fh=open(phenotype_filename, 'r')
@@ -16,13 +19,13 @@ def extract_traits(phenotype_filename, list_keywords):
         desc_pattern='^    "Description": '
         contain_desc=re.search(desc_pattern, i)
         if contain_desc:
-            next_pos_desc=(contain_desc.span())[-1] + 1
+            next_pos_desc=(contain_desc.span())[-1]
             for j in list_keywords:
                 if j in i[next_pos_desc:]:
                     extract=(re.search(r'.*$', i[next_pos_desc:])).group()
                     next_line=lines[x+1]
                     id_pattern='^    "Id": '
-                    next_pos_id=(re.search(id_pattern, next_line).span())[-1] + 1
+                    next_pos_id=(re.search(id_pattern, next_line).span())[-1]
                     id_value=next_line[next_pos_id:]
                     if extract in container:
                         container[extract].append([j, id_value])
@@ -34,10 +37,26 @@ def extract_traits(phenotype_filename, list_keywords):
 
 results_extraction=extract_traits('trait_list_BXDPublish.json', list_keywords)
 
+def save_info(filename, info):
+    """
+    Takes the filename to save data to and data info
+    Open file, if does not exist, creates
+    Writes each element of info in a separate line
+    """
+    fh=open(filename, 'w')
+    for element in info:
+        val=info[element]
+        fh.write(f'{element} -> {val}\n')
+    fh.close()
+    
+#save_info('BXD_traits_selected_info.txt', results_extraction)
 
 def retrieve_traits_id(traits_container):
     """
-    
+    Takes dictionary containing traits info
+    Extracts the trait id
+    Adds it to array traits_id_retrieved
+    Returns array
     """
     traits_id_retrieved=[]
     for value in traits_container.values():
@@ -47,4 +66,4 @@ def retrieve_traits_id(traits_container):
     
 
 list_ids=retrieve_traits_id(results_extraction)
-print(list_ids)
+#print(list_ids)
---
+++ transform_BXDPublish.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from extract_relevant_BXD_traits_id import list_ids # get list of traits ids
+
+import pandas as pd
+
+bxd_publish=pd.read_csv('BXDPublish.csv') # read in BXD phenotype file
+new_bxd_publish=bxd_publish.copy() # make a copy and work on it
+
+
+def process_data(data):
+    """
+    Takes dataframe of BXD phenotypes called data
+    Iterates through list of extracted traits ids and format items accordingly
+    Searches for formatted traits id in columns of phenotype dataframe
+    Saves phenotype found data in a new dataframe
+    Return new dataframe called new_data
+    """
+    new_data=pd.DataFrame()
+    for i in list_ids:
+        j=i.strip()
+        j=j.strip(', ')
+        j= f'BXD_{j}'
+        if j in data.columns:
+            new_data[j]=data[j]
+    return new_data
+
+new_to_save=process_data(new_bxd_publish)
+new_to_save.to_csv('TrimmedBXDPublish.csv') # save results in file
