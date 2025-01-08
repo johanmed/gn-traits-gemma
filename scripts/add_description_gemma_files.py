7ad49d5c3233b06b9d44792e2fdf90ec4376e335
Johannes Medagbe <johanmedagbe@gmail.com>
2025-01-08 21:26:46 +0300

    Improved add_description_gemma_files.py and new gemma2dataset.py added

--- scripts/add_description_gemma_files.py
+++ scripts/add_description_gemma_files.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import numpy as np
 import os
 
-gemma_files=[i for i in os.listdir() if 'assoc' in i and 'relevant' in i and 'new' not in i]
-gemma_files=sorted(gemma_files)
+os.chdir('../output')
 
-info_read=open('BXD_traits_selected_info.csv').readlines()
-info_read=sorted(info_read)
+gemma_files=[i for i in os.listdir() if ('assoc' in i) and ('relevant' in i) and ('new' not in i) and ('log' not in i)] # read files names from directory and store in array
+gemma_files=sorted(gemma_files) # sort by names
+
+info_read=open('../data/BXD_traits_selected_info.csv').readlines() # read contents of metadata file
 
 def add_desc_gemma_assoc(file, val1, val2):
+        """
+        Write to a new gemma file contents of old gemma file, val1 (trait category), val2(full description) tab separated
+        """
+        
         gemma_content=open(file).readlines()
         to_write=[]
         for u in gemma_content:
@@ -21,19 +25,34 @@ def add_desc_gemma_assoc(file, val1, val2):
         gemma_write.write(ready_to_write)
             
 def process_file(info_read, gemma_files, add_desc_gemma_assoc):
+    """
+    Process all gemma files creating a new file for each using metadata information to infer category of trait and full trait description
+    """
     for i, j in enumerate(info_read):
         x, y, z = j.split('\t')
+        c=''
+        if z.count(': ')==3:
+            a, b, d, c = z.split(': ')
+        elif z.count(': ')==2:
+            a, b, c = z.split(': ')
+        elif z.count(': ')==4:
+            a, b, d, e, c = z.split(': ')
+        elif z.count(': ')==5:
+            a, b, d, e, g, c = z.split(': ')
+            
+        phe=''.join(u for u in c if u.isalnum() or u==' ')
+        
         for f in gemma_files:
             o, p, q, r=f.split('_')
             l, m, n= r.split('.')
             if i==int(l[5:]) and y=='Diabetes trait':
-                print(f'Inferred diabetes trait for {f}')
-                add_desc_gemma_assoc(f, 0, z)
+                #print(f'Inferred diabetes trait for {f}')
+                add_desc_gemma_assoc(f, 0, phe)
             elif i==int(l[5:]) and y=='Immune system trait':
-                print(f'Inferred Immune system trait for {f}')
-                add_desc_gemma_assoc(f, 1, z)
+                #print(f'Inferred Immune system trait for {f}')
+                add_desc_gemma_assoc(f, 1, phe)
             elif i==int(l[5:]) and y=='Gut microbiome trait':
-                print(f'Inferred Gut microbiome trait for {f}')
-                add_desc_gemma_assoc(f, 2, z)
+                #print(f'Inferred Gut microbiome trait for {f}')
+                add_desc_gemma_assoc(f, 2, phe)
 
 process_file(info_read, gemma_files, add_desc_gemma_assoc)
